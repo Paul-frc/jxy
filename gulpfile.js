901d841e2e025ce08b73d6c0349d8525e4678f85
Paul Browne <paul.browne@frantic.com>
2017-03-24 08:56:07 +0200

    uses vanilla server with http2

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp        = require('gulp');
-var runSequence = require('run-sequence');
 var gulpIf      = require('gulp-if');
 var useref      = require('gulp-useref');
 var purify      = require('gulp-purifycss');
@@ -8,26 +7,57 @@ var uglify      = require('gulp-uglify');
 var htmlmin     = require('gulp-htmlmin');
 var imagemin    = require('gulp-imagemin');
 var cache       = require('gulp-cache');
+
+var fs          = require('file-system');
+var finalhandler = require('finalhandler');
+var http        = require('http');
+var http2       = require('http2');
+var serveStatic = require('serve-static');
+var runSequence = require('run-sequence');
 var del         = require('del');
+
+var connect     = require('gulp-connect');
 var browserSync = require('browser-sync');
 
 // Start browserSync server
-gulp.task('browserSync', function() {
-  browserSync({
-    server: {
-        baseDir: "./dist",
-        serveStaticOptions: {
-            extensions: ['html']
-        }
-    },
-    // https: {
-    //     key: "dist/localhost/key.pem",
-    //     cert: "dist/localhost/cert.pem"
-    // },
-    // httpModule: 'http2',
-    notify: false
-  })
+// gulp.task('browserSync', function() {
+//   browserSync({
+//     server: {
+//         baseDir: "./dist",
+//         serveStaticOptions: {
+//             extensions: ['html']
+//         }
+//     },
+//     // https: {
+//     //     key: "dist/localhost/key.pem",
+//     //     cert: "dist/localhost/cert.pem"
+//     // },
+//     // httpModule: 'http2',
+//     notify: false
+//   })
+// })
+
+// Serve up public/ftp folder
+// var serve = serveStatic('public/ftp', {'index': ['index.html', 'index.htm']})
+
+// Serve up public/ftp folder
+
+var options = {
+  key: fs.readFileSync('./.ssl/key.pem'),
+  cert: fs.readFileSync('./.ssl/cert.pem')
+};
+
+var serve = serveStatic('./dist', {
+  'index': ['index.html'],
+  'extensions': ['html']
 })
+
+// Create server + listen
+var server = http2.createServer(options, function onRequest (req, res) {
+  serve(req, res, finalhandler(req, res))
+}).listen(8888)
+
+
 
 // minify + uglify css, js and html
 // optimize images
@@ -41,7 +71,7 @@ gulp.task('all', function() {
       minifyCSS: true,
       minifyJS: true
     })))
-    //.pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.js', uglify()))
     .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulpIf('*.+(png|jpg|jpeg|gif|svg)', cache(imagemin({
       interlaced: true,
@@ -80,7 +110,7 @@ gulp.task('build', function(callback) {
 
 // Watch
 gulp.task('watch', function() {
-    gulp.watch('src/**/*.+(html|css|js)', ['build', browserSync.reload]);
+    gulp.watch('src/**/*.+(html|css|js)', ['build']);
 })
 
 // Gulp - Build + Watch
@@ -88,7 +118,6 @@ gulp.task('default', function(callback) {
   runSequence(
     'build',
     'watch',
-    'browserSync',
     callback
   )
 })
